5a489c9daf6c02db49347893ebe5a044ea745529
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2024-09-23 13:27:25 +0530

    Change position

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -10,7 +10,7 @@ function Background() {
         <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900">
           Notes.
         </h1>
-        <footer className="absolute top-[90%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold">
+        <footer className="absolute top-[80%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold">
           👨🏻‍💻 Developed by Sid
         </footer>
       </div>
